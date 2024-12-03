5d33e07354f186eaff5c6723790a7a74554da443
Liam Kuo <karllauy@gmail.com>
2024-12-03 23:05:56 +0800

    Setting up supabase

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,10 @@
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/views/HomeView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'Projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    }
-  ],
+  routes
 })
 
 export default router
