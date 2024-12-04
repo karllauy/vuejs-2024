22679c732ca87c88f52844fe3632d53907d7eb66
Liam Kuo <karllauy@gmail.com>
2024-12-04 15:43:19 +0800

    Setup UI library

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,29 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+
+import tailwind from 'tailwindcss'
+
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     VueRouter(),
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // Don't throw error for iconify-icon since it's a custom element
+          isCustomElement: (element) => element.startsWith('iconify-icon'),
+        }
+      }
+    }),
     vueDevTools(),
   ],
   resolve: {
